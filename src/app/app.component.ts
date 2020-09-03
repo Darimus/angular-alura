916f0ffaf86c641896fd8948a863869ab2140ea2
Darimus <bruno.passos@outlook.com.br>
2020-09-03 15:40:07 -0300

    :tada: Começando estudos de Angular, fizemos um data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  description = 'Liliana Vess';
+  url = 'https://i.pinimg.com/originals/2c/26/e8/2c26e86579d25cb108b94d21ca27bcf1.jpg';
 }
