3266adf4e056147d4bdc3e8e4c0ea473fc386696
Darimus <bruno.passos@outlook.com.br>
2020-09-15 16:58:51 -0300

    :triangular_flag_on_post: Onde parei na aula

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,11 +1,16 @@
 import { HttpClientModule } from "@angular/common/http";
-import { NgModule } from "@angular/core";
-import { PhotoComponent } from "./photo/photo.component";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 
 @NgModule({
     declarations: [ PhotoComponent, PhotoListComponent ],
-    imports: [ HttpClientModule ]
+    imports: [ 
+        HttpClientModule,
+        CommonModule
+    ]
 })
 
 export class PhotosModule {}
